9682954f7949be81ad9e3f5508453b34cd6e0a5b
Mihail Yonchev <myonchev99@gmail.com>
2019-05-18 20:43:42 +0100

    Implemented clearFields function on adding item to the list

--- app.js
+++ app.js
@@ -100,6 +100,19 @@ var UIController = (function() {
         },
 
 
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+        
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array){
+                current.value = "";
+            });
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -136,9 +149,11 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
-        // 4. Calculate Budget
+        // 4. Clear the fields
+        UICtrl.clearFields();
+        // 5. Calculate Budget
 
-        // 5. Display the Budget to the UI
+        // 6. Display the Budget to the UI
 
     };
 
