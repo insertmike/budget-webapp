761f0a0ee20dc29521d8da0a71c33e6e4c135894
Mihail Yonchev <myonchev99@gmail.com>
2019-05-18 12:19:04 +0100

    Implemented the Module Pattern

---
+++ app.js
@@ -0,0 +1,35 @@
+// Budget Controller
+var budgetController = (function(){
+
+    // To do 
+
+ })()
+
+// UI Controller
+var UIController = (function() {
+
+    // To do 
+
+})();
+
+// Global app controller
+var controller = (function(budgetCtrl, UICtrl) {
+
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        // 1. Get the field input data
+
+        // 2. Add Item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate Budget
+
+        // 5. Display the Budget to the UI
+
+    });
+
+    document.addEventListener('keypress', function(event) {
+        
+    });
+
+})(budgetController,UIController);
