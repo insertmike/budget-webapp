165593a801e84e4ebd5b25cb2650c142c4710141
Mihail Yonchev <myonchev99@gmail.com>
2019-05-18 16:50:48 +0100

    Implemented addItem function to budgetController

--- app.js
+++ app.js
@@ -13,16 +13,53 @@ var budgetController = (function(){
         this.value = value;
     };
 
- })()
 
- // Testing purposes
- var Expense = function(id,description,value) {
-    this.id = id;
-    this.description = description;
-    this.value = value;
-};
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
 
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
 
+            // Create new ID;
+            if(data.allItems[type].length > 0 ){
+            ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+           
+            
+            // Create new item based on 'inc' or 'exp' type
+            
+            if(type === 'exp') {
+                newItem = new Expense(ID,des,val);
+            } else if(type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return new element
+            return newItem;
+        },
+        
+        testing: function() {
+            console.log(data);
+        }
+    };
+
+ })();
+
+ 
 // UI Controller
 var UIController = (function() {
     var DOMstrings = {
@@ -68,11 +105,13 @@ var controller = (function(budgetCtrl, UICtrl) {
   
 
     var ctrlAddItem = function () {
+        var input, newItem;
+
         // 1. Get the field input data
-        var input = UIController.getInput();
+        input = UIController.getInput();
 
         // 2. Add Item to the budget controller
-
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI
 
         // 4. Calculate Budget
